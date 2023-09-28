71c0b9e471a9fcbc87a6a6c4c38b39443b6da81d
j-akhil <145708487+j-akhil@users.noreply.github.com>
2023-09-28 16:03:18 -0400

    Update TestMe.java

--- src/main/java/com/mycompany/app/TestMe.java
+++ src/main/java/com/mycompany/app/TestMe.java
@@ -1,41 +1,17 @@
-//package c;
-package com.mycompany.app;
-
+package c;
 
 public class TestMe {
-    private double value = Double.NaN;
+	private double value = Double.NaN;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
 
-    public TestMe() {}
+	public TestMe() {}
 
-    public double square() {
-        return value * value;
-    }
+	public double square() { return value*value; }
+	public void setValue(double v) {
+		value = v;
+	}
 
-<<<<<<< HEAD
-    public void setValue(double v) {
-        value = v;
-    }
-
-    public double add(double x) {
-        return value + x;
-    }
-
-    public double subtract(double x) {
-        return value - x;
-    }
-
-    public double multiply(double x) {
-        return value * x;
-    }
-
-    public double divide(double x) {
-        if (x != 0) {
-            return value / x;
-        } else {
-            throw new ArithmeticException("Division by zero");
-        }
-    }
+}
