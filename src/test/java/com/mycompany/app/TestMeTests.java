7508b331d40f2e5b67b0e4084acf000c9a55121f
akj20004 <akj20004@akj20004-vm.cse.uconn.edu>
2023-09-28 15:09:59 -0400

    update

--- src/test/java/com/mycompany/app/TestMeTests.java
+++ src/test/java/com/mycompany/app/TestMeTests.java
@@ -1,30 +1,65 @@
 //package u;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
-
 import com.mycompany.app.TestMe;
 
 public class TestMeTests {
 
-	TestMe testMe;
+    TestMe testMe;
 
-	@Before
-	public void setUp() throws Exception {
-		testMe  = new TestMe();
+    @Before
+    public void setUp() throws Exception {
+        testMe = new TestMe();
     }
 
-	@Test
-	public void squareTest() {
+    @Test
+    public void squareTest() {
+        double baseValue = 3.0;
+        double expected = baseValue * baseValue;
+        testMe.setValue(baseValue);
+        double actual = testMe.square();
+        assertEquals(expected, actual, 0.0001);
+    }
 
-		double baseValue = 3.0;
-		double expected = baseValue*baseValue;
-		testMe.setValue(3.0);
-		double actual = testMe.square();
+    @Test
+    public void addTest() {
+        double baseValue = 3.0;
+        double x = 2.0;
+        double expected = baseValue + x;
+        testMe.setValue(baseValue);
+        double actual = testMe.add(x);
+        assertEquals(expected, actual, 0.0001);
+    }
 
-		assertTrue(actual == expected);
-	}
+    @Test
+    public void subtractTest() {
+        double baseValue = 3.0;
+        double x = 2.0;
+        double expected = baseValue - x;
+        testMe.setValue(baseValue);
+        double actual = testMe.subtract(x);
+        assertEquals(expected, actual, 0.0001);
+    }
 
+    @Test
+    public void multiplyTest() {
+        double baseValue = 3.0;
+        double x = 2.0;
+        double expected = baseValue * x;
+        testMe.setValue(baseValue);
+        double actual = testMe.multiply(x);
+        assertEquals(expected, actual, 0.0001);
+    }
+
+    @Test
+    public void divideTest() {
+        double baseValue = 6.0;
+        double x = 2.0;
+        double expected = baseValue / x;
+        testMe.setValue(baseValue);
+        double actual = testMe.divide(x);
+        assertEquals(expected, actual, 0.0001);
+    }
 }
